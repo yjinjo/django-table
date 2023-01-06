1052ed10a77048da9debfce1f656ff2848b634a2
Youngjin Jo <yjinjo@berkeley.edu>
2023-01-06 21:01:02 +0900

    #2. 로또 번호 출력 페이지 만들기

--- first/views.py
+++ first/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-from django.template import loader
 from datetime import datetime
+
+import random
 
 
 # Create your views here.
@@ -11,20 +10,34 @@ def index(request):
     context = {
         'current_date': now
     }
-    # return HttpResponse("Hello world.")
-    # return HttpResponse(template.render(context, request))
     return render(request, 'first/index.html', context)
 
 
 def select(request):
-    # message = '수 하나를 입력해주세요.'
-    # return HttpResponse(message)
-    context = {'number': 4}
+    context = {}
     return render(request, 'first/select.html', context)
 
 
 def result(request):
-    # message = '추첨 결과입니다.'
-    # return HttpResponse(message)
-    context = {'numbers': [1, 2, 3, 4, 5, 6]}
+    chosen = int(request.GET['number'])  # number 라는 값으로 전달 받은 데이터를 꺼내옵니다.
+
+    results = []
+    # 만약 수가 범위를 초과 하지 않으면 결과 값에 미리 선택한 수를 넣습니다.
+    if 1 <= chosen <= 45:
+        results.append(chosen)
+
+    # 값을 꺼낼 박스를 마련합니다.
+    box = []
+    for num in range(1, 46):
+        if num != chosen:  # 사용자가 선택하지 않은 값만 박스에 넣습니다.
+            box.append(num)
+
+    # 랜덤하게 뒤섞습니다.
+    random.shuffle(box)
+
+    # results 개수가 6개가 될 때 까지 값을 하나씩 꺼냅니다.
+    while len(results) < 6:
+        results.append(box.pop())
+
+    context = {'numbers': results}
     return render(request, 'first/result.html', context)
