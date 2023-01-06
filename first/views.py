3fef209d3e01a25e13ad4fb5df3c815e733dae11
Youngjin Jo <yjinjo@berkeley.edu>
2023-01-06 15:38:12 +0900

    #2. 템플릿 조건 처리

--- first/views.py
+++ first/views.py
@@ -7,20 +7,24 @@ from datetime import datetime
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('index.html')
     now = datetime.now()
     context = {
         'current_date': now
     }
     # return HttpResponse("Hello world.")
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
 
 
 def select(request):
-    message = '수 하나를 입력해주세요.'
-    return HttpResponse(message)
+    # message = '수 하나를 입력해주세요.'
+    # return HttpResponse(message)
+    context = {'number': 4}
+    return render(request, 'select.html', context)
 
 
 def result(request):
-    message = '추첨 결과입니다.'
-    return HttpResponse(message)
+    # message = '추첨 결과입니다.'
+    # return HttpResponse(message)
+    context = {'numbers': [1, 2, 3, 4, 5, 6]}
+    return render(request, 'result.html', context)
