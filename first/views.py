545ac3844e9645258f473c9018499c2e0681ec3f
Youngjin Jo <yjinjo@berkeley.edu>
2023-01-06 20:19:46 +0900

    #2. static 파일 띄우기

--- first/views.py
+++ first/views.py
@@ -13,18 +13,18 @@ def index(request):
     }
     # return HttpResponse("Hello world.")
     # return HttpResponse(template.render(context, request))
-    return render(request, 'index.html', context)
+    return render(request, 'first/index.html', context)
 
 
 def select(request):
     # message = '수 하나를 입력해주세요.'
     # return HttpResponse(message)
     context = {'number': 4}
-    return render(request, 'select.html', context)
+    return render(request, 'first/select.html', context)
 
 
 def result(request):
     # message = '추첨 결과입니다.'
     # return HttpResponse(message)
     context = {'numbers': [1, 2, 3, 4, 5, 6]}
-    return render(request, 'result.html', context)
+    return render(request, 'first/result.html', context)
