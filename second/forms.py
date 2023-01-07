bbb62b98d288015bfc4a45bddf94609dbd346c99
Youngjin Jo <yjinjo@berkeley.edu>
2023-01-07 16:47:17 +0900

    #4. Model Form 사용하기

--- second/forms.py
+++ second/forms.py
@@ -1,6 +1,29 @@
-from django import forms
+# from django import forms
+from django.forms import ModelForm
+from second.models import Post
+from django.utils.translation import gettext_lazy as _
 
 
-class PostForm(forms.Form):  # django의 폼을 상속
-    title = forms.CharField(label='제목', max_length=200)
-    content = forms.CharField(label="내용", widget=forms.Textarea)
+#
+# class PostForm(forms.Form):  # django의 폼을 상속
+#     title = forms.CharField(label='제목', max_length=200)
+#     content = forms.CharField(label="내용", widget=forms.Textarea)
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post  # 어떤 모델 클래스를 사용할 것인지
+        fields = ['title', 'content']  # 입력받을 필드들
+
+        labels = {
+            'title': _('제목'),
+            'content': _('내용'),
+        }
+        help_texts = {
+            'title': _('제목을 입력해주세요.'),
+            'content': _('내용을 입력해주세요.'),
+        }
+        error_messages = {
+            'name': {
+                'max_length': _("제목이 너무 깁니다. 30자 이하로 해주세요."),
+            },
+        }
