53f56862f2dd133decc74f18cba93665ea519b90
Youngjin Jo <yjinjo@berkeley.edu>
2023-01-08 15:25:21 +0900

    #6. R: 페이지 구현

--- third/views.py
+++ third/views.py
@@ -1,9 +1,14 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from third.models import Restaurant
 
 
 def list(request):
-    context = {
-        'restaurants': Restaurant.objects.all()
-    }
-    return render(request, 'third/list.html', context)
+    restaurants = Restaurant.objects.all()
+    paginator = Paginator(restaurants, 5)  # 한 페이지에 5개씩 표시합니다.
+
+    page = request.GET.get("page")  # query params에서 page 데이터를 가져옵니다.
+    items = paginator.get_page(page)  # 해당 페이지의 아이템으로 필터링합니다.
+
+    context = {"restaurants": items}
+    return render(request, "third/list.html", context)
